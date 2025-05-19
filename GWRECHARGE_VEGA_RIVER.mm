4779bea36be29af554b0532b4367360c704da42f
khaors <khaors@gmail.com>
2025-05-18 22:04:54 -0500

    Mind map added

---
+++ GWRECHARGE_VEGA_RIVER.mm
@@ -0,0 +1,176 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="GWRECHARGE_VEGA_RIVER" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1746971308158"><hook NAME="MapStyle" background="#2e3440ff" zoom="1.1">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_tags="UNDER_NODES" show_note_icons="true" associatedTemplateLocation="template:/dark_nord_template.mm" fit_to_viewport="false" showTagCategories="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_671184412" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#eceff4" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#88c0d0" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_671184412" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" BORDER_WIDTH="1.9 px">
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ebcb8b">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" COLOR="#484747">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#5e81ac"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_779275544" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_779275544"/>
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#eceff4">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<node TEXT="INTRODUCTION" POSITION="bottom_or_right" ID="ID_195754216" CREATED="1745854673180" MODIFIED="1745854771835">
+<icon BUILTIN="full-1"/>
+<node TEXT="INTRODUCE TOPIC" ID="ID_1607223628" CREATED="1745854733191" MODIFIED="1745854737811"/>
+<node TEXT="WHAT DONE" ID="ID_186612424" CREATED="1745854739454" MODIFIED="1745854742666"/>
+<node TEXT="IDENTIFY THE GAP" ID="ID_1934794618" CREATED="1745854744350" MODIFIED="1745854747519"/>
+<node TEXT="INTRODUCE WORK" ID="ID_602441656" CREATED="1745854750107" MODIFIED="1745854753379"/>
+<node TEXT="MAIN CONTRIBUTION" ID="ID_427048588" CREATED="1745854754576" MODIFIED="1745854757454"/>
+<node TEXT="RESEARCH QUESTIONS" ID="ID_1590510171" CREATED="1746047564843" MODIFIED="1746047569862">
+<node TEXT="RECHARGE" ID="ID_1707524000" CREATED="1746047625678" MODIFIED="1746047628659">
+<node TEXT="MAGNITUDE" ID="ID_424957941" CREATED="1746047629332" MODIFIED="1746047632611"/>
+<node TEXT="ZONES" ID="ID_1618425774" CREATED="1746047633659" MODIFIED="1746047635700"/>
+<node TEXT="MONTHS" ID="ID_1732805833" CREATED="1746047636431" MODIFIED="1746047638421"/>
+</node>
+</node>
+<node TEXT="PAPER STRUCTURE" ID="ID_414091643" CREATED="1745854758330" MODIFIED="1745854763013"/>
+</node>
+<node TEXT="STUDY AREA" POSITION="top_or_left" ID="ID_779454231" CREATED="1745854680673" MODIFIED="1745854833354">
+<icon BUILTIN="full-2"/>
+<node TEXT="GENERAL DESCRIPTION" ID="ID_948037011" CREATED="1745854686010" MODIFIED="1745854917671">
+<node TEXT="LOCATION" ID="ID_1491822400" CREATED="1746971575750" MODIFIED="1746971579747"/>
+<node TEXT="MUNICIPALITIES" ID="ID_750014432" CREATED="1746971581283" MODIFIED="1746971584377"/>
+<node TEXT="POPULATION" ID="ID_1743551169" CREATED="1746971585903" MODIFIED="1746971589393"/>
+<node TEXT="WATER" ID="ID_644855698" CREATED="1746971598788" MODIFIED="1746971600372"/>
+</node>
+<node TEXT="CLIMATE" ID="ID_925240681" CREATED="1746971616290" MODIFIED="1746971617923">
+<node TEXT="RAINY SEASONS" ID="ID_1155765070" CREATED="1746971623994" MODIFIED="1746971628916">
+<node TEXT="TWO SEASONS" ID="ID_1397810040" CREATED="1746971659384" MODIFIED="1746971663283">
+<node TEXT="MARCH-MAY" ID="ID_10863510" CREATED="1746971664373" MODIFIED="1746971670069"/>
+<node TEXT="OCTOBER-NOVEMBER" ID="ID_215954313" CREATED="1746971671974" MODIFIED="1746971678153"/>
+</node>
+</node>
+<node TEXT="CLASSIFICATION" ID="ID_1040419854" CREATED="1746971645664" MODIFIED="1746971648349">
+<node TEXT="subtropical highland climate (Köppen Cfb)" ID="ID_1383836557" CREATED="1746971713035" MODIFIED="1746971928132"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <mark class="QVRyCf" style="background-position: 0% 0%; background-repeat: repeat; background-attachment: scroll; background-color: rgb(255, 255, 255); background-image: null; color: rgb(0, 29, 53); font-weight: 400; padding-top: 0px; padding-bottom: 0px; padding-right: 2px; padding-left: 2px; font-family: Google Sans, Arial, sans-serif; font-size: 18px; font-style: normal; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; word-spacing: 0px; white-space: normal;">
+      <span style="color: rgb(0, 29, 53); font-family: Google Sans, Arial, sans-serif; font-size: 18px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; word-spacing: 0px; white-space: normal; background-color: rgb(255, 255, 255);">a type of climate characterized by mild temperatures and significant seasonal rainfall differences, typically found in mountainous or elevated regions within the tropics or subtropics</span></mark><span style="color: rgb(0, 29, 53); font-family: Google Sans, Arial, sans-serif; font-size: 18px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; word-spacing: 0px; white-space: normal; background-color: rgb(255, 255, 255);">.&#xa0;It's known for mild winters, especially in the subtropics, and wet summers</span>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node TEXT="HYDROGEOLOGY" ID="ID_770474320" CREATED="1745854920540" MODIFIED="1745854923846"/>
+<node TEXT="GEOLOGY" ID="ID_1978466784" CREATED="1746971950304" MODIFIED="1746971953545"/>
+</node>
+<node TEXT="METHODOLOGY" POSITION="bottom_or_right" ID="ID_1239150093" CREATED="1745854688505" MODIFIED="1745854838831">
+<icon BUILTIN="full-3"/>
+<node TEXT="SOIL WATER BALANCE" ID="ID_1917399107" CREATED="1745854934489" MODIFIED="1745854937937">
+<node TEXT="EQUATIONS" ID="ID_689713588" CREATED="1745854939211" MODIFIED="1745854941794"/>
+</node>
+<node TEXT="INPUT DATA" ID="ID_1061766747" CREATED="1745854951621" MODIFIED="1745854953433">
+<node TEXT="SOIL VARIABLES" ID="ID_1655591684" CREATED="1745854982896" MODIFIED="1745854984967">
+<node TEXT="SOIL TYPE" ID="ID_1537477937" CREATED="1745854963100" MODIFIED="1745854964893"/>
+<node TEXT="LAND USE LAND COVER" ID="ID_1641214911" CREATED="1745854966356" MODIFIED="1745854969592"/>
+</node>
+<node TEXT="TOPOGRAPHICAL VARIABLE" ID="ID_1686619911" CREATED="1745855001806" MODIFIED="1745855007297">
+<node TEXT="SLOPE" ID="ID_1030215581" CREATED="1745854979527" MODIFIED="1745854981061"/>
+</node>
+<node TEXT="HYDROLOGICAL VARIABLES" ID="ID_452280665" CREATED="1745855014157" MODIFIED="1745855017301">
+<node TEXT="PRECIPITATION" ID="ID_1929563207" CREATED="1745855018246" MODIFIED="1745855020621"/>
+<node TEXT="EVT" ID="ID_895349107" CREATED="1745855021105" MODIFIED="1745855023020"/>
+</node>
+</node>
+</node>
+<node TEXT="RESULTS" POSITION="top_or_left" ID="ID_910698413" CREATED="1745854696016" MODIFIED="1745854841571">
+<icon BUILTIN="full-4"/>
+<node TEXT="GENERAL" ID="ID_86616962" CREATED="1745855063689" MODIFIED="1745855071376"/>
+<node TEXT="GEOLOGICAL UNITS" ID="ID_1483270240" CREATED="1745855072531" MODIFIED="1745855075855"/>
+<node TEXT="FIGS" ID="ID_322137475" CREATED="1745855081228" MODIFIED="1745855083096">
+<node TEXT="RECHARGE MAPS" ID="ID_230410871" CREATED="1745855084115" MODIFIED="1745855087455">
+<node TEXT="HYDROLOGICAL YEAR" ID="ID_696962535" CREATED="1745855112236" MODIFIED="1745855116515"/>
+</node>
+<node TEXT="BAR DIAGRAMS" ID="ID_239388623" CREATED="1745855123457" MODIFIED="1745855128561">
+<node TEXT="GEOLOGICAL UNIT" ID="ID_1049564041" CREATED="1745855129374" MODIFIED="1745855133958"/>
+<node TEXT="RUNOFF CLASSES?" ID="ID_1215823421" CREATED="1745855135440" MODIFIED="1745855148577"/>
+<node TEXT="LULC" ID="ID_856288392" CREATED="1745855150155" MODIFIED="1745855159319"/>
+</node>
+</node>
+</node>
+<node TEXT="DISCUSSION" POSITION="bottom_or_right" ID="ID_371851815" CREATED="1745854699328" MODIFIED="1745854844989">
+<icon BUILTIN="full-5"/>
+</node>
+<node TEXT="CONCLUSIONS" POSITION="top_or_left" ID="ID_1311051545" CREATED="1745854702093" MODIFIED="1745854847489">
+<icon BUILTIN="full-6"/>
+</node>
+</node>
+</map>
